a9ff1f9d37eb11e2dd9c24e565c089f140068e7b
Cynox <97021216+cynox01@users.noreply.github.com>
2022-04-20 00:26:02 +0200

    Maquettage de la  page user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,9 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Component\Routing\HttpFoundation\Request;
-// use app\Entity\User;
-// use app\Form\RegisterType;
+
 
 class UserController extends AbstractController
 {
